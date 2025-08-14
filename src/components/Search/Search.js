e0443638e5450aabc059916a7a360e51e5978b7c
Mansheeno <mansheeno@gmail.com>
2025-08-14 01:58:09 +0100

    update

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,20 +1,24 @@
-import React, { useState } from 'react';
-import { AsyncPaginate } from 'react-select-async-paginate';
-import { fetchCities } from '../../api/OpenWeatherService';
+import React, { useState, useEffect } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { fetchCities } from "../../api/OpenWeatherService";
 
 const Search = ({ onSearchChange }) => {
-  const [searchValue, setSearchValue] = useState(null);
+  // Default to Jos, Nigeria
+  const defaultCity = {
+    value: "9.8965 8.8583", // Lat & Lon for Jos
+    label: "Jos, NG",
+  };
+
+  const [searchValue, setSearchValue] = useState(defaultCity);
 
   const loadOptions = async (inputValue) => {
     const citiesList = await fetchCities(inputValue);
 
     return {
-      options: citiesList.data.map((city) => {
-        return {
-          value: `${city.latitude} ${city.longitude}`,
-          label: `${city.name}, ${city.countryCode}`,
-        };
-      }),
+      options: citiesList.data.map((city) => ({
+        value: `${city.latitude} ${city.longitude}`,
+        label: `${city.name}, ${city.countryCode}`,
+      })),
     };
   };
 
@@ -23,9 +27,14 @@ const Search = ({ onSearchChange }) => {
     onSearchChange(enteredData);
   };
 
+  // Automatically load Jos on first render
+  useEffect(() => {
+    onSearchChange(defaultCity);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <AsyncPaginate
-      placeholder="Search for cities"
+      placeholder="Search for Nigerian cities"
       debounceTimeout={600}
       value={searchValue}
       onChange={onChangeHandler}
